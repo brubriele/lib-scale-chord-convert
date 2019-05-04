1a6a6e286b6c14c306ac8439a8cd1f2789b2de70
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-05-04 11:58:29 -0300

    master - roda eslint

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,19 +4,19 @@ const libConvert = require("../lib/index")
 
 describe('Conversor de escalas', () => {
     describe('Escala Maior', () => {
-        it('Deve retornar escala maior de D de acordo semitom(num)', () => {
+        it('Deve retornar escala maior de D de acordo com semitom(num)', () => {
             expect(libConvert.majorScale("D", 0)).to.eql(['D', 'E', 'F#', 'G', 'A', 'B', 'C#']);
         });
-        it('Deve retornar escala maior de D# de acordo semitom(num)', () => {
+        it('Deve retornar escala maior de D# de acordo com semitom(num)', () => {
             expect(libConvert.majorScale("D#", 2)).to.eql(['F', 'G', 'A', 'A#', 'C', 'D', 'E']);
         });
-        it('Deve retornar escala maior de B de acordo semitom(num)', () => {
+        it('Deve retornar escala maior de B de acordo com semitom(num)', () => {
             expect(libConvert.majorScale("b", 5)).to.eql(['E','F#','G#','A','B','C#','D#']);
         });
     });
     describe('Escala Menor', () => {
-        it('Deve retornar escala menor de C de acordo semitom(num)', () => {
-            expect(libConvert.minorScale("C", 0)).to.eql(['C','D',"D#",'F','G','G#','A#']);
+        it('Deve retornar escala menor de C de acordo com semitom(num)', () => {
+            expect(libConvert.minorScale("C", -8)).to.eql(['C','D',"D#",'F','G','G#','A#']);
         });
     });
 
