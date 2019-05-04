f67f5a8ac53ab9fd065c2115ce277b763e50586f
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-05-04 01:45:09 -0300

    test - implementa novos testes para escala maior e menor

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,14 +3,22 @@ const expect = chai.expect;
 const libConvert = require("../lib/index")
 
 describe('Conversor de escalas', () => {
-    it('Deve retornar escala maior de D de acordo semitom(num)', () => {
-        expect(libConvert.majorScale("D", 0)).to.eql(['D', 'E', 'F#', 'G', 'A', 'B', 'C#']);
+    describe('Escala Maior', () => {
+        it('Deve retornar escala maior de D de acordo semitom(num)', () => {
+            expect(libConvert.majorScale("D", 0)).to.eql(['D', 'E', 'F#', 'G', 'A', 'B', 'C#']);
+        });
+        it('Deve retornar escala maior de D# de acordo semitom(num)', () => {
+            expect(libConvert.majorScale("D#", 2)).to.eql(['F', 'G', 'A', 'A#', 'C', 'D', 'E']);
+        });
+        it('Deve retornar escala maior de D# de acordo semitom(num)', () => {
+            expect(libConvert.majorScale("b", 5)).to.eql(['E','F#','G#','A','B','C#','D#']);
+        });
     });
-    it('Deve retornar escala maior de D# de acordo semitom(num)', () => {
-        expect(libConvert.majorScale("D#", 2)).to.eql(['F','G','A','A#','C','D','E']);
+    describe('Escala Menor', () => {
+        it('Deve retornar escala menor de C de acordo semitom(num)', () => {
+            expect(libConvert.minorScale("C", 0)).to.eql(['C','D',"D#",'F','G','G#','A#']);
+        });
     });
-    it('Deve retornar escala maior de D# de acordo semitom(num)', () => {
-        expect(libConvert.majorScale("D#", 2)).to.eql(['F','G','A','A#','C','D','E']);
-    });
+
 });
-   
+
