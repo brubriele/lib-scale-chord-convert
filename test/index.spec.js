a7a6101670801df2e447ea20dbd0e9b8a4b2e756
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-05-04 01:27:55 -0300

    test - module exports para funções agrupadas em object libConvert

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,16 @@
 const chai = require("chai");
 const expect = chai.expect;
-const convertMajorScale = require("../lib/index")
+const libConvert = require("../lib/index")
 
-describe('Conversor de acordes', () => {
-    it('Deve retornar escala maior', () => {
-        expect(convertMajorScale("D", 0)).to.eql(['D', 'E', 'F#', 'G', 'A', 'B', 'C#']);
+describe('Conversor de escalas', () => {
+    it('Deve retornar escala maior de D de acordo semitom(num)', () => {
+        expect(libConvert.majorScale("D", 0)).to.eql(['D', 'E', 'F#', 'G', 'A', 'B', 'C#']);
+    });
+    it('Deve retornar escala maior de D# de acordo semitom(num)', () => {
+        expect(libConvert.majorScale("D#", 2)).to.eql(['F','G','A','A#','C','D','E']);
+    });
+    it('Deve retornar escala maior de D# de acordo semitom(num)', () => {
+        expect(libConvert.majorScale("D#", 2)).to.eql(['F','G','A','A#','C','D','E']);
     });
 });
    
